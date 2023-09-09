f91a24885a73ddb4e68e680676a0425c7fd35080
Carl Gross <cagross@everlooksolutions.com>
2023-09-09 18:50:22 +0700

    Return data from network request to browser.

--- SpecialsC#.API/Program.cs
+++ SpecialsC#.API/Program.cs
@@ -16,24 +16,6 @@ var app = builder.Build();
 
 var httpClient = app.Services.GetRequiredService<IHttpClientFactory>().CreateClient();
 
-// Make the GET request and waitfor the response.
-var response = await httpClient.GetAsync("https://api.wordpress.org/plugins/info/1.0/woocommerce.json");
-if (response.IsSuccessStatusCode)
-{
-
-  System.Diagnostics.Debug.WriteLine("CAG: Success status code.");
-  // Read the content as a string
-  var json = await response.Content.ReadAsStringAsync();
-  System.Diagnostics.Debug.WriteLine("CAG: json obtained.");
-  System.Diagnostics.Debug.WriteLine(json);
-  var myResponse = JsonSerializer.Deserialize<MyResponseModel>(json);
-  string myValue = myResponse.name;
-  System.Diagnostics.Debug.WriteLine($"myProp value: {myValue}");
-}
-else
-{
-  System.Diagnostics.Debug.WriteLine($"HTTP request failed with status code: {response.StatusCode}");
-}
 
 app.UseSwagger();
 app.UseSwaggerUI();
@@ -47,12 +29,29 @@ app.MapControllers();
 // Ensure default route (i.e. homepage) returns static text.
 app.MapGet("/", async context =>
 {
-  await context.Response.WriteAsync("Future port of Grocery Specials web app to .NET based tech stack. See https://github.com/cagross/csharp-specials. See working Swagger page at https://specialscapi20230823213803.azurewebsites.net/swagger/index.html");
+  // Make the GET request and wait for the response.
+  var response = await httpClient.GetAsync("https://api.wordpress.org/plugins/info/1.0/woocommerce.json");
+  if (response.IsSuccessStatusCode)
+  {
+
+    System.Diagnostics.Debug.WriteLine("CAG: Success status code.");
+    // Read the content as a string
+    var json = await response.Content.ReadAsStringAsync();
+    System.Diagnostics.Debug.WriteLine("CAG: json obtained.");
+    System.Diagnostics.Debug.WriteLine(json);
+    var myResponse = JsonSerializer.Deserialize<MyResponseModel>(json);
+    string myValue = myResponse.version;
+    System.Diagnostics.Debug.WriteLine($"myProp value: {myValue}");
+    await context.Response.WriteAsync($"Future port of Grocery Specials web app to .NET based tech stack. See https://github.com/cagross/csharp-specials. See working Swagger page at https://specialscapi20230823213803.azurewebsites.net/swagger/index.html  Here is some sample text: {myValue}");
+  }
+  else
+  {
+    System.Diagnostics.Debug.WriteLine($"HTTP request failed with status code: {response.StatusCode}");
+  }
 });
 
 app.Run();
 public class MyResponseModel
 {
-  public string name { get; set; } // Replace 'string' with the actual data type of myProp in your JSON
-                                   // Add other properties matching your JSON structure
+  public string version { get; set; } // Replace 'string' with the actual data type of myProp in your JSON
 }
