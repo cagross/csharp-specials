af540b2be065dd72d8ef3055b1a31b36b4edb639
Carl Gross <cagross@everlooksolutions.com>
2023-09-17 12:36:21 +0700

    Add a simple end-to-end test.
    * Add xUnit testing framework.
    * Add Selenium test automation framework.

---
+++ SpecialsxUnitProject/GrocerySpecialsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+public class GrocerySpecialsTest : IDisposable
+{
+  private IWebDriver driver;
+
+  public GrocerySpecialsTest()
+  {
+    // Initialize the WebDriver (ChromeDriver in this case)
+    driver = new ChromeDriver();
+  }
+
+  [Fact]
+  public void CheckGrocerySpecialsText()
+  {
+    // Navigate to your web application's URL
+    driver.Navigate().GoToUrl("https://localhost:7165");
+
+    // Check if "Grocery Specials" text is present on the page
+    IWebElement element = driver.FindElement(By.XPath("//*[contains(text(),'Grocery Specials')]"));
+    Assert.NotNull(element);
+  }
+
+  public void Dispose()
+  {
+    // Dispose of the WebDriver when the test is done
+    driver.Quit();
+  }
+}
