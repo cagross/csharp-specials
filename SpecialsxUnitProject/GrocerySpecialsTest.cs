fe0e685fb38b509104ce2465b7b19c2c074ce487
Carl Gross <cagross@everlooksolutions.com>
2023-09-17 14:20:48 +0700

    Add radio buttons end-to-end test.

--- SpecialsxUnitProject/GrocerySpecialsTest.cs
+++ SpecialsxUnitProject/GrocerySpecialsTest.cs
@@ -13,6 +13,7 @@ public class GrocerySpecialsTest : IDisposable
     driver = new ChromeDriver();
   }
 
+  // Test that 'Grocery Specials' text is present on page.
   [Fact]
   public void CheckGrocerySpecialsText()
   {
@@ -22,6 +23,18 @@ public class GrocerySpecialsTest : IDisposable
     // Check if "Grocery Specials" text is present on the page
     IWebElement element = driver.FindElement(By.XPath("//*[contains(text(),'Grocery Specials')]"));
     Assert.NotNull(element);
+  }
+
+  // Test that radio buttons are present on page.
+  [Fact]
+  public void CheckRadioButtons()
+  {
+    // Navigate to your web application's URL
+    driver.Navigate().GoToUrl("https://localhost:7165");
+
+    // Check if there are four radio buttons on the page
+    IReadOnlyCollection<IWebElement> radioButtons = driver.FindElements(By.CssSelector("input[type='radio']"));
+    Assert.Equal(4, radioButtons.Count);
   }
 
   public void Dispose()
